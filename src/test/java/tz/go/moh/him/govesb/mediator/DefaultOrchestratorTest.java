5ec172025c58eeffe85f7f9fb471df401eeebf17
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2021-10-21 15:54:04 +0300

    added log4j

--- src/test/java/tz/go/moh/him/govesb/mediator/DefaultOrchestratorTest.java
+++ src/test/java/tz/go/moh/him/govesb/mediator/DefaultOrchestratorTest.java
@@ -4,22 +4,54 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.testkit.JavaTestKit;
+
+import java.io.File;
+import java.io.InputStream;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.*;
 import org.openhim.mediator.engine.MediatorConfig;
 import org.openhim.mediator.engine.messages.FinishRequest;
 import org.openhim.mediator.engine.messages.MediatorHTTPRequest;
+import org.openhim.mediator.engine.testing.MockLauncher;
+import org.openhim.mediator.engine.testing.TestingUtils;
 
 import static org.junit.Assert.*;
 
 public class DefaultOrchestratorTest {
-
+    /**
+     * Represents the system actor.
+     */
     static ActorSystem system;
 
+    /**
+     * Represents the configuration.
+     */
+    protected static MediatorConfig configuration;
+
+    /**
+     * Runs initialization before each class execution.
+     */
     @BeforeClass
-    public static void setup() {
-        system = ActorSystem.create();
+    public static void beforeClass() {
+        try {
+            configuration = loadConfig(null);
+            system = ActorSystem.create();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+//        List<MockLauncher.ActorToLaunch> actorsToLaunch = new LinkedList<>();
+//
+//        actorsToLaunch.add(new MockLauncher.ActorToLaunch("http-connector", MockDestination.class));
+//
+//        TestingUtils.launchActors(system, configuration.getName(), actorsToLaunch);
     }
 
     @AfterClass
@@ -39,8 +71,7 @@ public class DefaultOrchestratorTest {
     @Test
     public void testMediatorHTTPRequest() throws Exception {
         new JavaTestKit(system) {{
-            final MediatorConfig testConfig = new MediatorConfig("govesb-mediator", "localhost", 3105);
-            final ActorRef defaultOrchestrator = system.actorOf(Props.create(DefaultOrchestrator.class, testConfig));
+            final ActorRef defaultOrchestrator = system.actorOf(Props.create(DefaultOrchestrator.class, configuration));
 
             MediatorHTTPRequest POST_Request = new MediatorHTTPRequest(
                     getRef(),
@@ -79,5 +110,46 @@ public class DefaultOrchestratorTest {
 
             assertTrue("Must send FinishRequest", foundResponse);
         }};
+    }
+
+    /**
+     * Loads the mediator configuration.
+     *
+     * @param configPath The configuration path.
+     * @return Returns the mediator configuration.
+     */
+    public static MediatorConfig loadConfig(String configPath) {
+        MediatorConfig config = new MediatorConfig();
+
+
+        try {
+            if (configPath != null) {
+                Properties props = new Properties();
+                File conf = new File(configPath);
+                InputStream in = FileUtils.openInputStream(conf);
+                props.load(in);
+                IOUtils.closeQuietly(in);
+
+                config.setProperties(props);
+            } else {
+                config.setProperties("mediator.properties");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        config.setName(config.getProperty("mediator.name"));
+        config.setServerHost(config.getProperty("mediator.host"));
+        config.setServerPort(Integer.parseInt(config.getProperty("mediator.port")));
+        config.setRootTimeout(Integer.parseInt(config.getProperty("mediator.timeout")));
+
+        config.setCoreHost(config.getProperty("core.host"));
+        config.setCoreAPIUsername(config.getProperty("core.api.user"));
+        config.setCoreAPIPassword(config.getProperty("core.api.password"));
+
+        config.setCoreAPIPort(Integer.parseInt(config.getProperty("core.api.port")));
+        config.setHeartbeatsEnabled(true);
+
+        return config;
     }
 }
